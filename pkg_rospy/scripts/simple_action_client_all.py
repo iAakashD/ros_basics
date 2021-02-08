d8d0dcc8b427fc3ace85f7aa1699714f25c217b7
Aakash <dammala.aakash@gmail.com>
2021-02-08 21:43:32 +0530

    all functions of simple_action client server in python

---
+++ pkg_rospy/scripts/simple_action_client_all.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+# Sends goal and does its work, done_cb callback function is invoked whenever a 
+# msg on /count_numbers/result is received, feedback function is incoked 
+# whenever a msg on /count_numbers/feedback is received
+
+import rospy
+import actionlib
+from pkg_rospy.msg import CountNumbersAction, CountNumbersGoal
+
+class CountNumbersClient():
+    def __init__(self):
+        self._ac = actionlib.SimpleActionClient('/count_numbers', CountNumbersAction)
+        self._ac.wait_for_server()
+        rospy.loginfo("connected to actionserver")
+
+    def send_goal(self):
+        goal = CountNumbersGoal()
+        goal.count_upto = 10
+        goal.wait_time = 1.0
+        self._ac.send_goal(goal, done_cb = self.done_cb, feedback_cb=self.feedback_cb)
+        rospy.loginfo("Goal has been sent")
+
+        #rospy.sleep(1)     #uncomment to cancel goal while execution
+        #self._ac.cancel_goal()
+    
+    def done_cb(self, status, result):
+        rospy.loginfo("status is: " + str(status))
+        rospy.loginfo("result is: " + str(result))  #3 means success, 4 means aborted, reference: http://docs.ros.org/en/kinetic/api/actionlib_msgs/html/msg/GoalStatus.html
+        rospy.logwarn("Shutting down node")
+        rospy.signal_shutdown("Done Executing")
+
+    def feedback_cb(self, feedback):
+        rospy.loginfo("feedback is: " + str(feedback))
+
+def main():
+    rospy.init_node("count_numbers_client")
+    Client = CountNumbersClient()
+    Client.send_goal()
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
---
+++ pkg_rospy/scripts/simple_action_server_all.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+
+# Counts until count_upto with wait_time between each count, sends feedback after 
+# each count and sends the result on completion
+
+import rospy
+import actionlib
+from pkg_rospy.msg import CountNumbersAction, CountNumbersGoal, \
+        CountNumbersResult, CountNumbersFeedback
+
+class CountNumbersServer:
+    def __init__(self):
+        self._as = actionlib.SimpleActionServer('/count_numbers', CountNumbersAction, \
+                execute_cb = self.on_goal, auto_start=False)
+        self._as.start()
+        rospy.loginfo("count numbers server started")
+
+        self._counter = 0
+    
+    def on_goal(self, goal):
+        rospy.loginfo("Goal has be en received")
+        rospy.loginfo(goal)
+        count_upto = goal.count_upto
+        wait_time = goal.wait_time
+
+        self._counter = 0
+        rate = rospy.Rate(1.0/wait_time)
+
+        feedback = CountNumbersFeedback()
+        some_error_occoured = False
+        preempt_reuested = False
+        while (not rospy.is_shutdown()) and (self._counter < count_upto):
+            if some_error_occoured:     #counting has failed due to some error
+                break
+            if self._as.is_preempt_requested():
+                preempt_reuested = True
+                break
+
+            self._counter += 1
+
+            feedback.current_count = self._counter
+            self._as.publish_feedback(feedback)
+            
+            rate.sleep()
+        
+        result = CountNumbersResult()
+        result.final_count = self._counter
+        
+        if some_error_occoured:
+            self._as.set_aborted(result)
+            rospy.logwarn("Goal aborted")
+        elif preempt_reuested:
+            self._as.set_preempted(result)
+            rospy.logwarn("Goal Preempted")
+        else:
+            self._as.set_succeeded(result)
+            rospy.loginfo("Goal completed succesfully")
+
+def main():
+    rospy.init_node("count_numbers_server")
+    Server = CountNumbersServer()
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
