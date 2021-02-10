c98a1b733ff3c707f278dacb927511db88d97f6f
Aakash <dammala.aakash@gmail.com>
2021-02-10 20:26:06 +0530

    simple_action server and client in c++

---
+++ pkg_roscpp/src/simple_action_client.cpp
@@ -0,0 +1,44 @@
+#include "ros/ros.h"
+#include "actionlib/client/simple_action_client.h"
+#include "pkg_roscpp/CountNumbersAction.h"
+
+class ActionClient{
+protected:
+    ros::NodeHandle nh_;
+    actionlib::SimpleActionClient<pkg_roscpp::CountNumbersAction> ac_;
+
+public:
+    ActionClient() : 
+        ac_("count_numbers", true)
+    {
+        ROS_INFO("waiting for server to start");
+        ac_.waitForServer();    
+    }
+
+    ~ActionClient(void){}
+
+    void send_goal_get_result(){
+        ROS_INFO("connected to server");
+        pkg_roscpp::CountNumbersGoal goal;
+        goal.count_upto = 10;
+        goal.wait_time = 1.0;
+        ac_.sendGoal(goal, 
+                boost::bind(&ActionClient::done_CB, this, _1, _2)); //_1 _2 -> number of arguments in done_CB()
+        ROS_INFO("goal sent");
+    }
+
+    void done_CB(const actionlib::SimpleClientGoalState &state, 
+            const pkg_roscpp::CountNumbersResultConstPtr &result)
+    {
+        //ROS_INFO("Finished in state: %s", (state).toString.c_str());
+        ROS_INFO("Result: [%d]", (int)result->final_count);
+    }
+};
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "simple_action_client");
+    ActionClient Client;
+    Client.send_goal_get_result();
+    ros::spin();
+    return 0;
+}
---
+++ pkg_roscpp/src/simple_action_server.cpp
@@ -0,0 +1,50 @@
+#include "ros/ros.h"
+#include "actionlib/server/simple_action_server.h"
+#include "pkg_roscpp/CountNumbersAction.h"
+
+class ActionServer{
+protected:
+    ros::NodeHandle nh_;
+    actionlib::SimpleActionServer<pkg_roscpp::CountNumbersAction> as_;
+
+    std::string action_name_;
+    pkg_roscpp::CountNumbersFeedback feedback_;
+    pkg_roscpp::CountNumbersResult result_;    
+
+public:
+    ActionServer(std::string name) : 
+        as_(nh_, name, boost::bind(&ActionServer::executeCB, this, _1), false), 
+        action_name_(name)
+    {
+        as_.start();
+        ROS_INFO("Action Server started");
+    }
+
+    ~ActionServer(void){}
+
+    void executeCB(const pkg_roscpp::CountNumbersGoalConstPtr &goal){
+        int32_t count = 0;
+        int32_t count_upto = goal->count_upto;
+        float wait_time = goal->wait_time;
+        ROS_INFO("Goal received, count_upto: [%d], wait_time: [%f]", count_upto, wait_time);
+
+        ros::Rate rate(1.0/wait_time);
+
+        while (!ros::isShuttingDown() && count<count_upto){
+            count++;
+            rate.sleep();
+        }
+        result_.final_count = count;
+        as_.setSucceeded(result_);
+        ROS_INFO("Goal sent");
+    }
+};
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "simple_action_server");
+    ActionServer Server("count_numbers");
+
+    ros::spin();
+    
+    return 0;
+}
